dd88395ed793425a08020e8c96f53884b61f80d8
sadestyrahmadhani <sadestyrahmadhani012@gmail.com>
2023-05-16 12:37:11 +0700

    Add migration & authentication api

---
+++ src/database/seeders/20230515055630-user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { genSalt, hash } = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return queryInterface.bulkInsert(
+    'users', 
+    [
+      {
+        id: 'bd4faa79-04b8-4c89-8003-43330b1a9fc2',
+        name: 'Michael Subakier',
+        email: 'subakier07@gmail.com',
+        username: 'michael77',
+        password: await hash('1234', await genSalt()),
+        phone: '0812345678',
+        longitude: '-991882737723',
+        latitude: '199283777',
+        address: 'lorem ipsum dolor sit amet',
+        role: 'Visitor',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]
+   )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('users', null, {
+      truncate: true,
+      restartIdentity: true
+    })
+  }
+};

de39d2f7130ac2cd680d010a0ba398bf3a8e01ea
mirzasaputra <mirzasaputra066@gmail.com>
2023-06-10 20:19:26 +0700

    modified multer config

--- src/modules/v1/routes/index.js
+++ src/modules/v1/routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import multer from 'multer'
+import path from 'path'
 
 // controllers
 import { login, register, refreshToken, verifyToken } from '../controllers/auth/auth.controller'

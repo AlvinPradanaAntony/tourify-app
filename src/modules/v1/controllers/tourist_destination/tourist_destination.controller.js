cc7df03e94b7e1b1d209451733c1eb22ef57eabb
mirzasaputra <mirzasaputra066@gmail.com>
2023-06-10 22:24:14 +0700

    modified

--- src/modules/v1/controllers/tourist_destination/tourist_destination.controller.js
+++ src/modules/v1/controllers/tourist_destination/tourist_destination.controller.js
@@ -27,31 +27,36 @@ export const getAllTourDestination = async (req, res) => {
 export const storeTourDestination = async (req, res) => {
     try {
         if(!validateForm(req, res)) return 
-        const { tour_category_id, name, phone, email, price, description, address, longitude, latitude, day_open_start, day_open_end, time_open_start, time_open_end, user_id, picture } = req.body
-        
-        const tourDestination = await TouristDestination.create({
-            id: v4(),
-            tour_category_id: tour_category_id,
-            name: name,
-            phone: phone,
-            email: email,
-            price: price,
-            description: description,
-            address: address,
-            longitude: longitude,
-            latitude: latitude,
-            day_open_start: day_open_start,
-            day_open_end: day_open_end,
-            time_open_start: time_open_start,
-            time_open_end: time_open_end,
-            user_id: user_id,
-            picture: picture
+        const data = req.body.data.map((val, key) => {
+            const { tour_category_id, name, phone, email, price, description, address, longitude, latitude, day_open_start, day_open_end, time_open_start, time_open_end, user_id, picture } = val
+            
+            return {
+                id: v4(),
+                tour_category_id: tour_category_id,
+                name: name,
+                phone: phone,
+                email: email,
+                price: price,
+                description: description,
+                address: address,
+                longitude: longitude,
+                latitude: latitude,
+                day_open_start: day_open_start,
+                day_open_end: day_open_end,
+                time_open_start: time_open_start,
+                time_open_end: time_open_end,
+                user_id: user_id,
+                picture: picture,
+                updatedAt: new Date(),
+                createdAt: new Date(),
+            }
         })
 
+        await User.bulkCreate(data)
         res.status(200).json({
             status: 'success',
             message: 'Successfully creating data',
-            data: tourDestination
+            data: data
         })
     } catch(err) {
         res.status(500).json({

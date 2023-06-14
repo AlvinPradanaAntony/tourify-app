87d6e4d4c4fc8eecb143e061614250345fa58734
Developer <quickgaming321@gmail.com>
2023-06-14 14:53:41 +0700

    Update: Add Setting Page Design

--- app/src/main/java/com/devcode/tourifyapp/ui/customView/CustomDialog.kt
+++ app/src/main/java/com/devcode/tourifyapp/ui/customView/CustomDialog.kt
@@ -8,7 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.devcode.tourifyapp.R
 import com.devcode.tourifyapp.databinding.FragmentCustomDialogBinding
 
 
@@ -27,9 +30,10 @@ class CustomDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupAction()
+        captureRatingNFeedback()
     }
 
-    private fun setupAction(){
+    private fun setupAction() {
         binding.btnClose.setOnClickListener {
             dialog?.dismiss()
         }
@@ -39,10 +43,29 @@ class CustomDialog : DialogFragment() {
         val dialog = super.onCreateDialog(savedInstanceState)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         return dialog
+    }
 
+    private fun captureRatingNFeedback() {
+        var userRating = 0F
 
+        binding.rating.apply {
+            setOnRatingBarChangeListener { _, rating, _ ->
+                userRating = rating.also {
+                    val value = it.toInt()
+                    binding.tvRating.text = "${value}/5"
+                }
+            }
+        }
+
+        binding.btnSubmit.setOnClickListener {
+            if (binding.edAddDescription.text!!.isNotEmpty()) {
+                Toast.makeText(requireActivity(), "Rating ($userRating/5.0) And \n Feedback ${binding.edAddDescription.text}", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                binding.edAddDescription.error = "can't be blank"
+            }
+        }
     }
 
     companion object {
